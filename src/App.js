57d330c501c0f62e58d1c60f7937f8b1c21a0e12
Mason Kim <sungjinkimm@gmail.com>
2021-10-27 13:07:04 -0400

    completed build

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement, signIn, signOut } from './actions';
 
 function App() {
+  const counter = useSelector((state) => state.counter);
+  const isLogged = useSelector((state) => state.isLogged);
+  const dispatch = useDispatch();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>React - Redux </h1>
+
+      <hr />
+      <h3>{isLogged ? 'User Info (only visible when logged in)' : null}</h3>
+      <img src={logo} className='App-logo' alt='logo' />
+      {!isLogged ? (
+        <button id='signIn' onClick={() => dispatch(signIn())}>
+          Sign In
+        </button>
+      ) : (
+        <button id='signOut' onClick={() => dispatch(signOut())}>
+          Sign Out
+        </button>
+      )}
+      <hr />
+      <h2>Counter: {counter}</h2>
+      <button id='increment' onClick={() => dispatch(increment())}>
+        Increment +
+      </button>
+      <button id='decrement' onClick={() => dispatch(decrement())}>
+        Decrement -
+      </button>
+      <hr />
+      <div className='footer'>
+        <p>Built with Create-React-App & Redux</p>
+      </div>
     </div>
   );
 }
